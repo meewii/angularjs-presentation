52c97aeef5f129d91c903b31c3f4f52ac1dd5160
Andreas Marek <andimarek@fastmail.fm>
2013-09-11 21:56:55 +0200

    removed default content, added the prez

---
+++ examples/scripts/app.js
@@ -0,0 +1,44 @@
+function Example2ACtrl($scope) {
+  $scope.sayHello = 'girls!';
+}
+function Example2BCtrl($scope) {
+  $scope.sayHello = 'boys!';
+}
+
+
+function Example3Ctrl($scope) {
+  $scope.val1 = 0;
+  $scope.val2 = 0;
+}
+
+
+function Example4Ctrl($scope) {
+  $scope.pilots = [
+  	{
+  		codename: "Apollo",
+  		firstname: "Lee",
+  		lastname: "Adama"
+  	},
+  	{
+  		codename: "Starbuck",
+  		firstname: "Kara",
+  		lastname: "Thrace"
+  	},
+  	{
+  		codename: "Helo",
+  		firstname: "Karl",
+  		lastname: "Agathon"
+  	},
+  	{
+  		codename: "Kat",
+  		firstname: "Louanne",
+  		lastname: "Katraine"
+  	},
+  	{
+  		codename: "Boomer",
+  		firstname: "Sharon",
+  		lastname: "Valerii"
+  	}
+  ];
+}
+
